db0ff7afafda04eb9e9b992f056dbb9e5814de52
19krikma <31541643+19krikma@users.noreply.github.com>
2021-01-05 13:47:24 -0500

    Create Player.java

---
+++ Java8/Version1.0.0/Source Code/Player.java
@@ -0,0 +1,15 @@
+package MusicPlayer;
+
+import javafx.scene.media.*;
+
+public class Player {
+
+	Media media;
+	AudioClip mediaPlayer;
+	MediaView view;
+
+	public Player(String name) {
+		media = new Media(name);
+		mediaPlayer = new AudioClip(media.getSource());
+	}
+}
